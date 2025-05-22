c726652be3d8714d245974ebac8b6ac41022a47c
Iurii Mednikov <yurimednikov@proton.me>
2025-05-22 08:38:55 +0200

    Initial commit

---
+++ src/main/java/dev/mednikov/taskpal/TaskpalApplication.java
@@ -0,0 +1,13 @@
+package dev.mednikov.taskpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskpalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskpalApplication.class, args);
+    }
+
+}
